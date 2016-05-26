3b1250248e76996c3b56c4aee7c1908e7cede208
dmitriy <FFKL@yandex.ru>
2016-05-26 22:50:07 +0300

    Add authorization

--- src/org/chat/server/XMLParser.java
+++ src/org/chat/server/XMLParser.java
@@ -1,6 +1,9 @@
 package org.chat.server;
 
+import org.chat.Account;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -8,16 +11,20 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XMLParser {
 
     Document doc;
+    List<Account> accounts = new ArrayList<>();
 
     public XMLParser() {
         try {
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             doc = dBuilder.parse(new File("resources.xml"));
+            this.packAccounts();
         } catch (ParserConfigurationException e) {
             e.printStackTrace();
         } catch (SAXException e) {
@@ -37,5 +44,19 @@ public class XMLParser {
 
     public Integer getConnectionsCount() {
         return Integer.parseInt(doc.getElementsByTagName("connections-count").item(0).getTextContent());
+    }
+
+    public List<Account> getAccounts() {
+        return this.accounts;
+    }
+
+    private void packAccounts() {
+        NodeList accounts = doc.getElementsByTagName("account");
+        for (int i = 0; i < accounts.getLength(); i++) {
+            NodeList account = accounts.item(i).getChildNodes();
+            String login = account.item(1).getTextContent();
+            String password = account.item(3).getTextContent();
+            this.accounts.add(new Account(login, password));
+        }
     }
 }
